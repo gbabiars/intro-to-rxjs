797517b1ff721b18bda8776c14cbb0172e108ba2
Greg Babiars <gbabiars@gmail.com>
2016-06-26 09:41:44 -0700

    Add directory browsing for static assets

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 'use strict';
 
 const Hapi = require('hapi');
+const inert = require('inert');
 
 const server = new Hapi.Server();
 
-server.connection({
-  host: 'localhost',
-  port: 8000
-});
-
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler(request, reply) {
-    return reply('Hello World');
-  }
-});
-
-server.start(err => {
+server.register(inert, err => {
   if(err) {
     throw err;
   }
 
-  console.log(`Server running at: ${server.info.uri}`);
+  server.connection({
+    host: 'localhost',
+    port: 8000
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        listing: true
+      }
+    }
+  });
+
+  server.start(err => {
+    if(err) {
+      throw err;
+    }
+
+    console.log(`Server running at: ${server.info.uri}`);
+  });
 });
