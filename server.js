72547887b053345338f1ab085d6467850fc39920
Greg Babiars <gbabiars@gmail.com>
2016-06-26 09:26:32 -0700

    Add initial server

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler(request, reply) {
+    return reply('Hello World');
+  }
+});
+
+server.start(err => {
+  if(err) {
+    throw err;
+  }
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
