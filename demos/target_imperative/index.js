3805b4aac8ccd6f6deb879950b46d8e488faac5b
Greg Babiars <gbabiars@gmail.com>
2016-07-16 08:13:36 -0700

    Imperative target clicks: throttle posts to server

--- demos/target_imperative/index.js
+++ demos/target_imperative/index.js
@@ -4,15 +4,21 @@ const target = document.getElementById('target');
 const count = document.getElementById('count');
 const message = document.getElementById('message');
 
-target.addEventListener('click', () => {
-  clickCount += 1;
-  count.textContent = clickCount;
-
-  $.ajax({
+const postClicks = function(clickCount) {
+  return $.ajax({
     url: '/api/clicks',
     method: 'POST',
     data: {
       clicks: clickCount
     }
   }).then(text => message.textContent = text);
+};
+
+const throttledPostClicks = _.throttle(postClicks, 2000);
+
+target.addEventListener('click', () => {
+  clickCount += 1;
+  count.textContent = clickCount;
+
+  throttledPostClicks(clickCount);
 });
