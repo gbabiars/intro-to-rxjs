e8c025cb0e6513ee1a8c3dcffd914b13b1d9d0de
Greg Babiars <gbabiars@gmail.com>
2016-10-04 20:40:13 -0700

    WIP form refactor

--- demos/form/index.js
+++ demos/form/index.js
@@ -1,4 +1,7 @@
 const Observable = Rx.Observable;
+const { fromEvent, merge, combineLatest, ajax, of } = Observable;
+
+const log = data => console.log(data);
 
 
 // elements
@@ -9,12 +12,7 @@ const formMessage = document.getElementById('form-message');
 const nameMessage = document.getElementById('name-message');
 const emailMessage = document.getElementById('email-message');
 
-
-// helper functions
-function getValueForInput(input) {
-  return input.value.trim()
-}
-
+// validation helpers
 function validateName(name) {
   if(name === '') {
     return 'Name cannot be blank';
@@ -24,6 +22,7 @@ function validateName(name) {
   }
   return '';
 }
+
 function validateEmail(email) {
   if(email === '') {
     return 'Email cannot be blank';
@@ -34,85 +33,153 @@ function validateEmail(email) {
   return '';
 }
 
+function renderNameErrorMessage(err) {
+  nameMessage.textContent = err;
+}
 
-// form submit and response
-const formSubmit = Observable.fromEvent(form, 'submit')
+function renderEmailErrorMessage(err) {
+  emailMessage.textContent = err;
+}
+
+function renderFormErrorMessage(err) {
+  formMessage.textContent = err;
+}
+
+
+// name
+const nameBlur$ = fromEvent(name, 'blur')
+  .map(() => true)
+  .startWith(false)
+  .distinctUntilChanged();
+const nameKeyup$ = fromEvent(name, 'keyup');
+const nameKeypress$ = fromEvent(name, 'keypress');
+const nameChange$ = merge(nameKeyup$, nameKeypress$);
+const nameValue$ = nameChange$
+  .map(event => event.target.value.trim())
+  .startWith('')
+  .distinctUntilChanged();
+const nameClientState$ = combineLatest(
+  nameBlur$,
+  nameValue$,
+  (blur, value) => ({
+    blur,
+    value,
+    error: validateName(value)
+  })
+);
+
+// email
+const emailBlur$ = fromEvent(email, 'blur')
+  .map(() => true)
+  .startWith(false)
+  .distinctUntilChanged();
+const emailKeyup$ = fromEvent(email, 'keyup');
+const emailKeypress$ = fromEvent(email, 'keypress');
+const emailChange$ = merge(emailKeyup$, emailKeypress$);
+const emailValue$ = emailChange$
+  .map(event => event.target.value.trim())
+  .startWith('')
+  .distinctUntilChanged();
+const emailClientState$ = combineLatest(
+  emailBlur$,
+  emailValue$,
+  (blur, value) => ({
+    blur,
+    value,
+    error: validateEmail(value)
+  })
+);
+
+
+// form events
+const formData$ = combineLatest(
+  nameClientState$,
+  emailClientState$,
+  (name, email) => ({ name, email })
+);
+const formSubmit$ = fromEvent(form, 'submit')
   .do(event => event.preventDefault())
   .share();
+const formSubmitWithState$ = formSubmit$
+  .withLatestFrom(
+    formData$,
+    (event, data) => data
+  );
+const formValidSubmitWithState$ = formSubmitWithState$
+  .filter(({ name, email }) => name.error === '' && email.error === '');
+const formRequestValues$ = formValidSubmitWithState$
+  .map(({ name, email }) => ({ name: name.value, email: email.value }));
 
-const response = formSubmit
-  .map(() => {
-    return {
-      name: getValueForInput(name),
-      email: getValueForInput(email)
-    };
-  })
-  .filter(({ name, email }) => {
-    return validateName(name) === '' && validateEmail(email) === '';
-  })
-  .mergeMap(data => Observable.ajax({
+
+// ajax
+const response$ = formRequestValues$
+  .switchMap(data =>
+    ajax({
       url: '/api/contacts',
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
-    })
-      .catch(ajaxError => Observable.of(ajaxError))
+    }).catch(ajaxError => of(ajaxError))
   )
   .share();
-
-const responseSuccess = response
-  .filter(res => res.status === 200)
-  .map(res => res.response);
-const responseValidationErrors = response
+const serverValidationErrors$ = response$
   .filter(res => res.status === 400 && res.xhr.response.validation)
   .map(res => res.xhr.response.validation);
-const responseServerErrors = response
-  .filter(res => res.status !== 200 && res.status !== 400);
+const serverFormErrors$ = response$
+  .filter(res => res.status !== 200 && res.status !== 400)
+  .map(res => res.xhr.statusText);
 
 
-// field level validation
-function createFieldValidationObservable(element, validationFn, submit, serverMessages) {
-  const trackingState = Observable.merge(
-    Observable.fromEvent(element, 'blur'),
-    submit
-  );
-  const values = Observable.fromEvent(element, 'keyup')
-    .startWith(null)
-    .map(() => getValueForInput(element))
-    .distinctUntilChanged();
-  const clientMessages = values.map(validationFn);
-  const combinedMessages = clientMessages.merge(serverMessages);
-  const combinedMessagesWithTracking = combinedMessages.combineLatest(
-    trackingState,
-    message => message
-  );
-  return combinedMessagesWithTracking;
-}
 
-const nameErrors = createFieldValidationObservable(
-  name,
-  validateName,
-  formSubmit,
-  responseValidationErrors.map(errors => errors.name)
-);
-const emailErrors = createFieldValidationObservable(
-  email,
-  validateEmail,
-  formSubmit,
-  responseValidationErrors.map(errors => errors.email)
-);
+// combine errors
+const nameServerErrors$ = serverValidationErrors$
+  .map(errors => errors.name);
+const nameClientErrors$ = nameClientState$
+  .map(state => state.error);
+const nameErrors$ = merge(nameClientErrors$, nameServerErrors$)
+  .distinctUntilChanged();
+
+const emailServerErrors$ = serverValidationErrors$
+  .map(errors => errors.email);
+const emailClientErrors$ = emailClientState$
+  .map(state => state.error);
+const emailErrors$ = merge(emailClientErrors$, emailServerErrors$)
+  .distinctUntilChanged();
 
 
-// subscribe
-nameErrors.subscribe(message => nameMessage.innerText = message);
-emailErrors.subscribe(message => emailMessage.innerText = message);
-formSubmit.subscribe(() => formMessage.innerText = '');
-responseServerErrors.subscribe(() => formMessage.innerText = 'An error occurred while saving');
-responseSuccess.subscribe(data => {
-  form.remove();
-  let successMessage = document.createElement('div');
-  successMessage.innerText = `${data.name} was created!`;
-  document.body.appendChild(successMessage);
-});
+form$.subscribe();
+
+
+
+// const response = formSubmit
+//   .map(() => {
+//     return {
+//       name: getValueForInput(name),
+//       email: getValueForInput(email)
+//     };
+//   })
+//   .filter(({ name, email }) => {
+//     return validateName(name) === '' && validateEmail(email) === '';
+//   })
+//   .mergeMap(data => Observable.ajax({
+//       url: '/api/contacts',
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//       .catch(ajaxError => Observable.of(ajaxError))
+//   )
+//   .share();
+//
+// const responseSuccess = response
+//   .filter(res => res.status === 200)
+//   .map(res => res.response);
+// const responseValidationErrors = response
+//   .filter(res => res.status === 400 && res.xhr.response.validation)
+//   .map(res => res.xhr.response.validation);
+// const responseServerErrors = response
+//   .filter(res => res.status !== 200 && res.status !== 400);
